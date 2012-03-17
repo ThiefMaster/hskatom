f4e72fefe4282f07724a91b0887af97e8a2f8703
Adrian <adrian@planetcoding.net>
2012-03-17 14:31:37 +0100

    Retrieve and show news items

--- hskatom/__init__.py
+++ hskatom/__init__.py
@@ -1,6 +1,9 @@
 # vim: fileencoding=utf8
 
 import re
+import urllib
+from bs4 import BeautifulSoup
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask.ext.cache import Cache
 from werkzeug.contrib.atom import AtomFeed
@@ -29,7 +32,31 @@ app.url_map.converters['newskey'] = NewsKeyConverter
 @cache.memoize(NEWS_CACHE_DURATION)
 def retrieve_news(key):
     url = NEWS_URLS[key]
-    return None
+    app.logger.debug('Loading news from %s' % url)
+    try:
+        conn = urllib.urlopen(url)
+    except IOError:
+        return None
+    bs = BeautifulSoup(conn.read(), 'lxml')
+    conn.close()
+    content = bs.find('div', id='content')
+    if not content:
+        return None
+    blocks = content.find_all('div', 'csc-default')
+    items = []
+    for block in blocks:
+        item = {}
+        item['id'] = block['id']
+        item['title'] = block.find('h4').text
+        tmp = block.find('div', 'last')
+        if not tmp:
+            continue
+        tmp = tmp.find('div', 'content')
+        if not tmp:
+            continue
+        item['content'] = ''.join(map(unicode, tmp.contents))
+        items.append(item)
+    return items, datetime.utcnow()
 
 # View functions
 @app.route('/')
@@ -38,7 +65,13 @@ def index():
 
 @app.route('/<newskey:key>.atom')
 def atom(key):
-    retrieve_news(key)
-    feed = AtomFeed(FEED_TITLE, feed_url=request.url, url=request.host_url,
-        subtitle=key)
+    items, last_update = retrieve_news(key)
+    url = NEWS_URLS[key]
+    feed = AtomFeed(FEED_TITLE, feed_url=request.url, url=url,
+        subtitle=key, updated=last_update)
+    ts = datetime(1970, 1, 1)
+    for item in items:
+        item_url = '%s?1#%s' % (url, item['id'])
+        feed.add(item['title'], item['content'], content_type='html', id=item_url,
+            updated=ts, url=item_url)
     return feed.get_response()
